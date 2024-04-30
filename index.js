c6843e8c8616fce4daaff926c499b035dda1c0f6
imsabiya <sabiya.t@technovert.com>
2024-04-30 16:23:26 +0530

    initial

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+
 
 app.post("/webhook", (req, res) => {
   try {
