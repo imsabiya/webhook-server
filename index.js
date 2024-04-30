30dd0bb62281cdb4ff49ec79fa44a116f9436928
imsabiya <sabiya.t@technovert.com>
2024-04-30 16:52:38 +0530

    webhook setup

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+
+const {
+  Onfido,
+  Region,
+  OnfidoApiError,
+  Report,
+  WebhookEventVerifier,
+} = require("@onfido/api");
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-
 app.post("/webhook", (req, res) => {
   try {
-    console.log(req, req.body.payload,"req");
+    console.log(req, req.body.payload, "req");
     const payload = req.body.payload;
     console.log("Received webhook payload:", payload);
+
+    const webhookToken = "z2N2WUMM3Pb4vbAIcDwo-2XCxtZ6mLtY";
+    const signature = req.headers["x-sha2-signature"];
+    const rawEventBody = req.body.payload;
+    console.log(rawEventBody, "rawEventBody");
+    let response;
+    // const Event = readWebhookEvent({
+    //   rawEventBody,
+    //   signature,
+    //   webhookToken,
+    // });
+
+    const verifier = new WebhookEventVerifier(webhookToken);
+    try {
+      const event = verifier.readPayload(
+        JSON.stringify(rawEventBody),
+        signature
+      );
+      console.log(event, "event123");
+    } catch (error) {
+      // TODO: Handle invalid webhooks.
+      // console.log(' Onfido Webhook Verification Failed ', error);
+      return null;
+    }
+
     res.status(200).send("Webhook received successfully");
   } catch (err) {
     console.log(err);
