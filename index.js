61c4b08079874551a9a67210a06a0b69a57b70f7
imsabiya <sabiya.t@technovert.com>
2024-04-30 16:06:29 +0530

    initial

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.post("/webhook", (req, res) => {
     console.log(req);
     const payload = req.body;
     console.log("Received webhook payload:", payload);
-    res.status(200).send("Webhook received successfully");
+    res.status(403).send("Webhook received successfully");
   } catch (err) {
     console.log(err);
     res.status(400).send("error");
