5c83fb50722f7b19a320b0b68543a9ff242c10e8
imsabiya <sabiya.t@technovert.com>
2024-04-30 15:54:07 +0530

    initial

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.post("/webhook", (req, res) => {
+  try {
+    console.log(req);
+    const payload = req.body;
+    console.log("Received webhook payload:", payload);
+    res.status(200).send("Webhook received successfully");
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("error");
+  }
+});
+
+const port = process.env.PORT || 8002;
+app.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
+});
