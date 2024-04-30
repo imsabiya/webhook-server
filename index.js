d7b58010ac7c82bb2e4398c5268210d4454cc177
imsabiya <sabiya.t@technovert.com>
2024-04-30 16:11:45 +0530

    initial

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.post("/webhook", (req, res) => {
   try {
     //console.log(req);
-    const payload = req.body;
-    console.log("Received webhook payload:", payload);
-    res.status(403).send("Webhook received successfully");
+    const payload = req;
+    console.log("Received webhook payload:", JSON.stringify(payload));
+    res.status(200).send("Webhook received successfully");
   } catch (err) {
     console.log(err);
     res.status(400).send("error");
